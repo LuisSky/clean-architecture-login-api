87aeb94c3597bc4bea611127a82c3a381057b410
LuisSky <luizguilhermeabrantes@gmail.com>
2020-10-20 22:07:10 -0300

    add test of EmailValidator Class

--- src/routes/utils/email-validator.spec.js
+++ src/routes/utils/email-validator.spec.js
@@ -1,13 +1,33 @@
+const validator = require('validator')
 
 class EmailValidator {
   isValid (email) {
-    return true
+    return validator.isEmail(email)
   }
 }
+
+const makeSut = () => {
+  return new EmailValidator()
+}
+
 describe('EmailValidator test', () => {
   test('Should return true if valid email provider', () => {
-    const sut = new EmailValidator()
+    const sut = makeSut()
     const isValidEmail = sut.isValid('valid_email@mail.com')
     expect(isValidEmail).toBe(true)
+  })
+
+  test('Should return false if invalid email provider', () => {
+    validator.isEmailValid = false
+    const sut = makeSut()
+    const isValidEmail = sut.isValid('invalid_email@mail.com')
+    expect(isValidEmail).toBe(false)
+  })
+
+  test('Should call validator whit correct email', () => {
+    const sut = makeSut()
+    sut.isValid('any_email@mail.com')
+
+    expect(validator.email).toBe('any_email@mail.com')
   })
 })
