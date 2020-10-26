1a61943bd08e3aa2889ca1b6eb30f46557304b8a
LuisSky <luizguilhermeabrantes@gmail.com>
2020-10-25 22:43:12 -0300

    Ensure AuthUseCase return throw MissingParamError

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -1,7 +1,9 @@
+const { MissingParamsError } = require('../../utils/errors')
+
 class AuthUseCase {
   async auth (email) {
     if (!email) {
-      throw new Error()
+      throw new MissingParamsError('email')
     }
   }
 }
@@ -10,6 +12,6 @@ describe('AuthUseCase', () => {
   test('Should throw if no email is provided', async () => {
     const sut = new AuthUseCase()
     const promise = sut.auth()
-    expect(promise).rejects.toThrow()
+    expect(promise).rejects.toThrow(new MissingParamsError('email'))
   })
 })
