951d023ad9488a40513899cac72f74b2020bb466
LuisSky <luizguilhermeabrantes@gmail.com>
2020-11-25 08:32:51 -0300

    change AuthUseCase to receive just one object

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -47,7 +47,11 @@ const makeSut = () => {
   const loadUserByEmailRepositorySpy = makeLoadUserByEmail()
   const tokenGeneratorSpy = makeTokenGenerator()
 
-  const sut = new AuthUseCase(loadUserByEmailRepositorySpy, encrypterSpy, tokenGeneratorSpy)
+  const sut = new AuthUseCase({
+    loadUserByEmailRepository: loadUserByEmailRepositorySpy,
+    encrypter: encrypterSpy,
+    tokenGenerator: tokenGeneratorSpy
+  })
   return {
     sut,
     loadUserByEmailRepositorySpy,
@@ -76,13 +80,13 @@ describe('AuthUseCase', () => {
   })
 
   test('Should trhows if LoadUserByEmailRepository has no load function', async () => {
-    const sut = new AuthUseCase({})
+    const sut = new AuthUseCase({ loadUserByEmailRepository: {} })
     const promise = sut.auth('any_email@mail.com', 'any_password')
     expect(promise).rejects.toThrow(new InvalidParamsError('loadUserByEmailRepository'))
   })
 
   test('Should trhows if no LoadUserByEmailRepository is provided', async () => {
-    const sut = new AuthUseCase()
+    const sut = new AuthUseCase({})
     const promise = sut.auth('any_email@mail.com', 'any_password')
     expect(promise).rejects.toThrow(new MissingParamsError('loadUserByEmailRepository'))
   })
