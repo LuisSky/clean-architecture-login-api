6237ed1d951ddaa31264025d85aafec6c144dc1f
LuisSky <luizguilhermeabrantes@gmail.com>
2020-12-06 22:36:20 -0300

    ensure LoadUserByEmailRepository throws if no userModel is provided

--- src/infra/repositories/load-user-by-email-repository.spec.js
+++ src/infra/repositories/load-user-by-email-repository.spec.js
@@ -45,4 +45,9 @@ describe('LoadUserByEmailRepository', () => {
       password: fakeUser.ops[0].password
     })
   })
+  test('Should throw if no userModel is provided', async () => {
+    const sut = new LoadUserByEmailRepository()
+    const promise = sut.load('any_email@mail.com')
+    expect(promise).rejects.toThrow()
+  })
 })
